01a3de2e50b73ce50968c681b2d7d5b33dd90648
fingerskier <fingerskier@gmail.com>
2013-04-03 13:47:59 -0600

    Updated middleware config to 3.0   using Stylus & Jade   need to add Coffee support

--- app.js
+++ app.js
@@ -1,55 +1,39 @@
-var express = require('express'),
-  routes = require('./routes'),
-  socket = require('./routes/socket.js'),
-  styles = require('stylus');
+var express = require('express')
+,  routes = require('./routes')
+,  socket = require('./routes/socket.js')
+,  styles = require('stylus')
+;
 
 var app = module.exports = express();
 var server = require('http').createServer(app);
-
-// Hook Socket.io into Express
 var io = require('socket.io').listen(server);
 
-// Configuration
 app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(stylus.middleware({
-    src: __dirname + '/views', // .styl files are located in `views/stylesheets`
-    dest: __dirname + '/public', // .styl resources are compiled `/stylesheets/*.css`
-    compile: function (str, path, fn) { // optional, but recommended
-      stylus(str)
-      .set('filename', path)
-      .set('compress', true)
-      .render(fn);
-    }
-  }));
+  app.use(express.favicon());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
   app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(__dirname + '/public'));
 });
-
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
-
 app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
-
-// redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
-
-// Socket.io Communication
 
 io.sockets.on('connection', socket);
 
-// Start server
-
-server.listen(3000, function(){
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
